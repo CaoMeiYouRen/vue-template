c6a1bb4e40b19eb019536a739adc88d909eddbc7
草梅友仁 <996881204@qq.com>
2020-07-10 15:47:57 +0800

    更新依赖

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     publicPath: '/',
     devServer: {
-        open: true, //自动打开浏览器
-        port: 5000, //设置端口
-        hot: true, //启用热更新
-        compress: true, //是否启用gzip压缩
+        open: true, // 自动打开浏览器
+        port: 5000, // 设置端口
+        hot: true, // 启用热更新
+        compress: true, // 是否启用gzip压缩
     },
-    productionSourceMap: false, //移除生产环境的 source map
+    productionSourceMap: false, // 移除生产环境的 source map
 }
