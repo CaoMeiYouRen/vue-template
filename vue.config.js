b9621fd44d4603e45b4f4552442d5bc0d358f654
CaoMeiYouRen <996881204@qq.com>
2020-10-05 13:49:01 +0800

    update: 更新 CI；更新编译配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const pkg = require('./package.json')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+
 module.exports = {
     publicPath: '/',
     devServer: {
@@ -6,5 +9,22 @@ module.exports = {
         hot: true, // 启用热更新
         compress: true, // 是否启用gzip压缩
     },
-    productionSourceMap: false, // 移除生产环境的 source map
+    productionSourceMap: process.env.NODE_ENV === 'development', // 移除生产环境的 source map
+    chainWebpack: (config) => {
+        config.plugin('html').tap(([options]) => {
+            options.title = pkg.name
+            return [options]
+        })
+    },
+    configureWebpack: config => {
+        config.plugins.push(
+            // @ts-ignore
+            new StyleLintPlugin({
+                files: ['src/**/*.{vue,html,css,scss,sass,less}'],
+                failOnError: false,
+                cache: true,
+                fix: true,
+            }),
+        )
+    }
 }
